b9cd3b8ec653ff771d6d5f019836dd0654e87493
Yefga <yefga.tp@icloud.com>
2020-03-21 18:54:35 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Scroll The Stack
+
+Best practice to create view inside scrollView with embeded in StackView
+created in XCode 11 - XIB
---
+++ ScrollTheStack/views/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ScrollTheStack
+//
+//  Created by Yefga on 21/03/20.
+//  Copyright © 2020 Yefga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Scroll The Stack"
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
